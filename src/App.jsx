1a264dbcdca4c088011697871fa6c77754cecfa5
Erika Rubi Calderon Arias <ecalderona@hmujer.saludtab.gob.mx>
2023-06-19 12:47:55 -0600

    verificando el package.json por el build que tenia de mas

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   };
   return (
     <>
-      <h1 className="text-3xl font-bold">Lista de Tareas</h1>
+      <h1 className="text-lg text-black font-semibold">Lista de Tareas</h1>
       <TaskForm addTask={addTask} />
       <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />
     </>
